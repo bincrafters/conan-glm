abf6554bf140a7ad0fed98092c2e9c4b2366c8da
Croydon <cr0ydon@outlook.com>
2019-03-30 11:59:11 +0100

    Update conventions

--- conanfile.py
+++ conanfile.py
@@ -9,25 +9,27 @@ class GlmConan(ConanFile):
     name = "glm"
     version = "0.9.9.4"
     description = "OpenGL Mathematics (GLM)"
+    topics = ("conan", "glm", "opengl", "mathematics")
     url = "https://github.com/bincrafters/conan-glm"
     homepage = "https://github.com/g-truc/glm"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "MIT"
-    exports_sources = "platform.h.patch"
-    exports = ["FindGLM.cmake", "LICENSE.md"]
-    source_subfolder = "source_subfolder"
     no_copy_source = True
 
+    exports = ["FindGLM.cmake", "LICENSE.md"]
+    exports_sources = "platform.h.patch"
+
+    _source_subfolder = "source_subfolder"
+
     def source(self):
-        source_url = "https://github.com/g-truc/glm"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256="3a073eb8f3be07cee74481db0e1f78eda553b554941e405c863ab64de6a2e954")
         extracted_dir = "glm-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
         self.copy("FindGLM.cmake")
-        self.copy("*", src=os.path.join(self.source_subfolder, "glm"), dst=os.path.join("include", "glm"))
-        self.copy("copying.txt", dst="licenses", src=self.source_subfolder)
+        self.copy("*", src=os.path.join(self._source_subfolder, "glm"), dst=os.path.join("include", "glm"))
+        self.copy("copying.txt", dst="licenses", src=self._source_subfolder)
 
     def package_id(self):
         self.info.header_only()
