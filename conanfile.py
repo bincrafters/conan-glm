6f340cea63339a33b45a962529f7e94294f840a3
Uilian Ries <uilianries@gmail.com>
2019-05-22 14:39:17 -0300

    Fix support for GCC7
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -11,23 +11,26 @@ class GlmConan(ConanFile):
     url = "https://github.com/bincrafters/conan-glm"
     homepage = "https://github.com/g-truc/glm"
     author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "glm", "math", "opengl", "mathematics")
     license = "MIT"
-    exports_sources = "platform.h.patch"
-    exports = ["FindGLM.cmake", "LICENSE.md"]
-    source_subfolder = "source_subfolder"
+    exports_sources = "0001-gcc7.patch"
+    exports = ["LICENSE.md"]
     no_copy_source = True
 
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
     def source(self):
-        source_url = "https://github.com/g-truc/glm"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = "glm-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
-        tools.patch(base_path=self.source_subfolder, patch_file="platform.h.patch")
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+        tools.patch(base_path=self._source_subfolder, patch_file="0001-gcc7.patch")
 
     def package(self):
-        self.copy("FindGLM.cmake")
-        self.copy("*", src=os.path.join(self.source_subfolder, "glm"), dst=os.path.join("include", "glm"))
-        self.copy("copying.txt", dst="licenses", src=self.source_subfolder)
+        for pattern in ["*.h", "*.hpp", "*.inl"]:
+            self.copy(pattern, src=os.path.join(self._source_subfolder, "glm"), dst=os.path.join("include", "glm"))
+        self.copy("copying.txt", dst="licenses", src=self._source_subfolder)
 
     def package_id(self):
         self.info.header_only()
